36aa920f300baf4e7a13f0de45d6c644f0a60b8f
norber01 <dovinorbert@gmail.com>
2023-08-13 08:42:54 -0700

    AirBnB

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Explain the Amenity class."""
+from models.base_model import BaseModel
+
+
+
+class Amenity(BaseModel):
+    ""Shows an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Explains the State class."""
+from models.base_model import BaseModel
+
+
+
+class State(BaseModel):
+    """Shows a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
